6689ea1d50674717118f50953e8c64f60ced2af4
FBEZ <francesco.bez@gmail.com>
2016-05-12 13:55:06 +0200

    first commit try

--- parsehtml.py
+++ parsehtml.py
@@ -16,14 +16,14 @@ except urllib2.HTTPError:
 soup = BeautifulSoup(html,'html.parser')
 pronuncia = [row.text for row in soup.find_all('div', class_='headpron')]
 significato = soup.find_all('span', class_ = 'definition')
-print significato[0]
-
-
-print pronuncia[0]
-# div_pronuncia = soup.find("div", { "class" : "headpron" })
-# print div_pronuncia
-# div_significato = soup.find("div", { "class" : "senses" })
-# print div_significato
-
-# for link in soup.find_all('a'):
-#     print(link.get('href'))
+#print significato[0].find_all('a', class_='w translation')
+print
+print
+sign = ""
+for link in significato[1]:
+    try:
+        sign = sign+link.text
+    except AttributeError:
+        sign = sign+link
+print "Significato:\n************\n "+sign
+print "\n"+pronuncia[0]
