6bd875eb7446c06df10b83705764ff22758f9928
FBEZ <francesco.bez@gmail.com>
2016-05-12 19:26:49 +0200

    piu' significati

--- parsehtml.py
+++ parsehtml.py
@@ -16,14 +16,21 @@ except urllib2.HTTPError:
 soup = BeautifulSoup(html,'html.parser')
 pronuncia = [row.text for row in soup.find_all('div', class_='headpron')]
 significato = soup.find_all('span', class_ = 'definition')
-#print significato[0].find_all('a', class_='w translation')
+
 print
 print
-sign = ""
-for link in significato[1]:
-    try:
-        sign = sign+link.text
-    except AttributeError:
-        sign = sign+link
-print "Significato:\n************\n "+sign
+sign_array = []
+for s in significato:
+    sign = ""
+    for link in s:
+        try:
+            sign = sign+link.text
+        except AttributeError:
+            sign = sign+link
+    sign_array.append(sign)
+
+print "Significato:\n************\n "
+for s in sign_array:
+    print s
+    print
 print "\n"+pronuncia[0]
