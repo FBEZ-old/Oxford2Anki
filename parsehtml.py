f383696f0ec9887122f0f873660e69aee20fcd0a
FBEZ <francesco.bez@gmail.com>
2016-05-12 20:48:26 +0200

    generalizzato

--- parsehtml.py
+++ parsehtml.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
-import urllib2
+import urllib2,urllib,time,os
+import vlc
 from bs4 import BeautifulSoup
 print "Oxford2Anki"
 parola = raw_input("Parola:  ")
@@ -15,22 +16,32 @@ except urllib2.HTTPError:
 
 soup = BeautifulSoup(html,'html.parser')
 pronuncia = [row.text for row in soup.find_all('div', class_='headpron')]
-significato = soup.find_all('span', class_ = 'definition')
+significato = soup.find_all('div', class_ = 'senseInnerWrapper')
 
-print
-print
-sign_array = []
 for s in significato:
-    sign = ""
-    for link in s:
-        try:
-            sign = sign+link.text
-        except AttributeError:
-            sign = sign+link
-    sign_array.append(sign)
+    try:
+        print "* "+s.find_all('span',class_= 'definition')[0].text
+        print
+        i = 0
+        for example in s.find_all('span',class_='exampleGroup exGrBreak'):
+            i=i+1
+            print str(i)+")"+ example.find_all('em',class_='example')[0].text
+        print "------------------------------------"
+        print
+    except IndexError:
+        print
+print
+print
 
-print "Significato:\n************\n "
-for s in sign_array:
-    print s
-    print
 print "\n"+pronuncia[0]
+
+
+urllib.urlretrieve ("https://ssl.gstatic.com/dictionary/static/sounds/de/0/"+parola+".mp3", parola+".mp3")
+instance = vlc.Instance()
+
+player = instance.media_player_new()
+media = instance.media_new(parola+'.mp3')
+player.set_media(media)
+player.play()
+time.sleep(3)
+os.remove(parola+".mp3")
